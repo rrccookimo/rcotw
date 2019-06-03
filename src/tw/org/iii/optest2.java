12a94bb241ceb9fa201a284aacc2bcc946b5ed3b
rrccookimo <rrccookimo@users.noreply.github.com>
2019-06-03 17:23:43 +0800

    OK

---
+++ src/tw/org/iii/optest2.java
@@ -0,0 +1,27 @@
+package tw.org.iii;
+
+public class optest2 {
+
+	public static void main(String[] args) {
+		int var1 = 10; int var2 = 3;
+		int var3;
+		
+		var3 = var1 + var2;
+		System.out.printf("10 + 3 = %d\n", var3);
+		
+		var3 = var1 - var2;
+		System.out.printf("10 - 3 = %d\n", var3);
+
+		var3 = var1 * var2;
+		System.out.printf("10 * 3 = %d\n", var3);
+
+		var3 = var1 / var2;
+		System.out.printf("10 / 3 = %d\n", var3);
+		
+		var3 = var1 % var2;
+		System.out.printf("10 %% 3 = %d\n", var3);
+
+
+	}
+
+}
