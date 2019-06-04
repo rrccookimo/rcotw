517943594ef78cb705e7a41a80a617f86ef41b0b
rrccookimo <rrccookimo@users.noreply.github.com>
2019-06-04 18:07:59 +0800

    ok

--- src/tw/org/iii/optest4.java
+++ src/tw/org/iii/optest4.java
@@ -3,7 +3,14 @@ package tw.org.iii;
 public class optest4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		byte var1 = 10;
+		short var2 = 3;
+		//short var3 = var1 + var2; 	// byte, short, char, int ==> int
+		int var3 = var1 + var2;
+		long var4 = var1 + var2;	//long
+		float var5 = var1 + var2;	//float
+		double var6 = var1 + var2;	//double
+		
 
 	}
 
---
+++ src/tw/org/iii/optest5.java
@@ -0,0 +1,14 @@
+package tw.org.iii;
+
+public class optest5 {
+	
+	public static void main(String[] args) {
+		byte var1;
+		short var2 = 129;
+		var1 = (byte)var2;
+		System.out.println(var1);
+		
+		//byte, short, char, int, long, float, double
+	
+	}
+}
