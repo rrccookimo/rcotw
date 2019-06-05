b040178a24baa28bab7cfc1ce036ac6bc011517a
rrccookimo <rrccookimo@users.noreply.github.com>
2019-06-05 18:12:06 +0800

    ok

---
+++ src/tw/org/iii/IfTest2.java
@@ -0,0 +1,18 @@
+package tw.org.iii;
+
+public class IfTest2 {
+
+	public static void main(String[] args) {
+		int score = 50;
+		
+		if (score >= 60) {
+			System.out.println("Pass");
+			
+		}else {
+			System.out.println("Down");
+		}
+		System.out.println("End");
+
+	}
+
+}
